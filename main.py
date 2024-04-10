908e3a2c6d6cf282d3320c57900875ca3cf0c2ca
sydobd <obaidulhaq20@gmail.com>
2024-04-10 22:54:39 +0500

    created toolbar widget

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
 
 
-class Main(QWidget):
+class Main(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Product Manger")
@@ -15,8 +16,26 @@ class Main(QWidget):
         self.show()
 
     def UI(self):
-        pass
+        self.toolBar()
 
+    def toolBar(self):
+        self.tb = self.addToolBar("Tool Bar")
+        self.tb.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        # Toolbar #
+        # Add product #
+        self.addProduct = QAction(QIcon('icons/add.png'), "Add Product",self)
+        self.tb.addAction(self.addProduct)
+        self.tb.addSeparator()
+
+        # Add Member #
+        self.addMember = QAction(QIcon('icons/users.png'), "Add Member", self)
+        self.tb.addAction(self.addMember)
+        self.tb.addSeparator()
+
+        # Sell Product #
+        self.sellProduct = QAction(QIcon('icons/sell.png'), "Sell Product", self)
+        self.tb.addAction(self.sellProduct)
+        self.tb.addSeparator()
 
 def main():
     App = QApplication(sys.argv)
