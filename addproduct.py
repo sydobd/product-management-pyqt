41b6fe1f1113453f7dc27ab1b94d5bc72b443f49
sydobd <obaidulhaq20@gmail.com>
2024-04-11 21:59:00 +0500

    created widgets for add prod window

--- addproduct.py
+++ addproduct.py
@@ -18,7 +18,26 @@ class AddProduct(QWidget):
         self.show()
 
     def UI(self):
-        self.layouts
+        self.widgets()
+        self.layouts()
+
+    def widgets(self):
+        # top layout #
+        self.addProductImg = QLabel()
+        self.img = QPixmap('icons/addproduct.png')
+        self.addProductImg.setPixmap(self.img)
+        self.titleText = QLabel("Add Product")
+        # bottom layout #
+        self.nameEntry = QLineEdit()
+        self.nameEntry.setPlaceholderText("Enter name of product")
+        self.manufacturerEntry = QLineEdit()
+        self.manufacturerEntry.setPlaceholderText("Enter name of manufacturer")
+        self.priceEntry = QLineEdit()
+        self.priceEntry.setPlaceholderText("Enter price of product")
+        self.qoutaEntry = QLineEdit()
+        self.qoutaEntry.setPlaceholderText("Enter qouta of product")
+        self.uploadBtn = QPushButton("Upload")
+        self.submitBtn = QPushButton("Submit")
 
     def layouts(self):
         self.mainLayout = QVBoxLayout()
@@ -26,3 +45,20 @@ class AddProduct(QWidget):
         self.bottomLayout = QFormLayout()
         self.topFrame = QFrame()
         self.bottomFrame = QFrame()
+        # widgets top layout #
+        self.topLayout.addWidget(self.addProductImg)
+        self.topLayout.addWidget(self.titleText)
+        self.topFrame.setLayout(self.topLayout)
+        # widgets form layout #
+        self.bottomLayout.addRow(QLabel("Name: "), self.nameEntry)
+        self.bottomLayout.addRow(QLabel("Manufacturer: "), self.manufacturerEntry)
+        self.bottomLayout.addRow(QLabel("Price: "), self.priceEntry)
+        self.bottomLayout.addRow(QLabel("Qouta: "), self.qoutaEntry)
+        self.bottomLayout.addRow(QLabel("Upload: "), self.uploadBtn)
+        self.bottomLayout.addRow(QLabel(""), self.submitBtn)
+        self.bottomFrame.setLayout(self.bottomLayout)
+
+        self.mainLayout.addWidget(self.topFrame)
+        self.mainLayout.addWidget(self.bottomFrame)
+        self.setLayout(self.mainLayout)
+
