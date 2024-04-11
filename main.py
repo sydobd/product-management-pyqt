f30cb150aa38b4b23763d273cd4f093b5c72d41b
sydobd <obaidulhaq20@gmail.com>
2024-04-11 21:21:03 +0500

    created and connected db

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
+import sqlite3
+
+con = sqlite3.connect("products.db")
+cur = con.cursor()
 
 
 class Main(QMainWindow):
